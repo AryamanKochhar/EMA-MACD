a1ffcf683bf2af5ba8bfffac99b0549babbeeb67
Aryaman Kochhar <aryamankochhar456@gmail.com>
2024-05-22 19:49:13 +0530

    FUNCTIONSUSNINGENDpts

--- brokerusingapi.py
+++ brokerusingapi.py
@@ -1,6 +1,7 @@
 # The REST baseurl for testnet is "https://testnet.binancefuture.com"
 # The Websocket baseurl for testnet is "wss://fstream.binancefuture.com"
-
+#apikey =88e262c5697a9971f573a9c078d0956ad1bc5fb77a81eeae535952c8f8635637
+#apisecretkey=b27ce9a1ee11b3cad52b1a7248222feb1394c39c3896fb1c9868ded2bf3a1a6e
 import requests
 
 base_url="https://testnet.binancefuture.com"
@@ -32,5 +33,5 @@ def recenttradelist():
     reqrecent=requests.get(url="https://testnet.binancefuture.com/fapi/v1/trades",params=paratameters.get("symbol"))
     return reqrecent
 def login_function():
-    
+    reqlogin=requests.post(url)
     
